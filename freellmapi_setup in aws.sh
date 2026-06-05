077123d5ca32f5841279bdee0fe2d3561e90dd91
Md  Riam Madber <ahmedriam00268@gmail.com>
2026-06-05 13:14:03 +0600

    Update freellmapi_setup in aws.sh

--- freellmapi_setup in aws.sh
+++ freellmapi_setup in aws.sh
@@ -1,1 +1,13 @@
 
+#Prerequisites: Node.js 20+, npm.
+
+git clone https://github.com/tashfeenahmed/freellmapi.git
+cd freellmapi
+npm install
+cp .env.example .env
+ENCRYPTION_KEY="$(node -e 'console.log(require("crypto").randomBytes(32).toString("hex"))')"
+printf "ENCRYPTION_KEY=%s\nPORT=3001\n" "$ENCRYPTION_KEY" > .env
+npm run dev
+
+#open your actual .env
+nano ~/freellmapi/.env

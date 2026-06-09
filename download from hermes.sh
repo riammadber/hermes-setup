e33586a49720181eb4de9e9ede39141d2f7152d8
Md  Riam Madber <ahmedriam00268@gmail.com>
2026-06-09 15:00:45 +0600

    Update download from hermes.sh

--- download from hermes.sh
+++ download from hermes.sh
@@ -2,4 +2,7 @@
 sudo apt install zip -y  # install sudo 
 # go the project directory  
 cd /home/ubuntu/Hermes-project #examples 
-zip -r linkdrip.zip LinkDrip
+zip -r linkdrip.zip LinkDrip --exclude "LinkDrip/node_modules/*"
+
+#  SCP to your local machine (run this on your local PC)
+scp -i your-key.pem ubuntu@your-ec2-ip:/home/ubuntu/Hermes-project/LinkDrip.zip ./

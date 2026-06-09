3b40013ace0226da353572c9412ac406de1df89e
Md  Riam Madber <ahmedriam00268@gmail.com>
2026-06-09 14:52:30 +0600

    Update backup and push in github.sh

--- backup and push in github.sh
+++ backup and push in github.sh
@@ -1,2 +1,10 @@
 # for backup create a Fine-grained personal access tokens with content read and write permission 
 hermes config set GITHUB_BACKUP_TOKEN= your token
+
+# go folder directory 
+cd /home/ubuntu/Hermes-project/LinkDrip
+git init
+git add .
+git commit -m "LinkDrip project"
+git remote add origin https://github.com/yourusername/linkdrip.git
+git push -u origin main

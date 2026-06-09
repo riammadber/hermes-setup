c86ea294f2e9d34f7b11534ef2e84d5cec98a61e
Md  Riam Madber <ahmedriam00268@gmail.com>
2026-06-09 09:16:36 +0600

    Create venv.sh

---
+++ venv.sh
@@ -0,0 +1,10 @@
+# finds every venv by its config file. For a cleaner output with just the venv directories
+find / -name "pyvenv.cfg" 2>/dev/null | xargs -I{} dirname {}
+#delete broken venv
+#The pattern is:rm -rf /path/to/project/{folder_to_delete}
+rm -rf /home/ubuntu/.hermes/hermes-agent/{venv name}
+# Create fresh venv
+python3 -m venv /home/ubuntu/.hermes/hermes-agent/{venv_name}
+
+# Install hermes into it
+/home/ubuntu/.hermes/hermes-agent/{venv_name}/bin/pip install -e /home/ubuntu/.hermes/hermes-agent/

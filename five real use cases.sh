d3ed7a95bc635128cc3ebc2c9c2596b8ee10f7f6
Md  Riam Madber <ahmedriam00268@gmail.com>
2026-06-12 17:13:23 +0600

    Create five real use cases.sh

---
+++ five real use cases.sh
@@ -0,0 +1,26 @@
+#Trend rader
+Every mornin 7am scan Reddit, X, and AI forums, identify what workflows are gaining velocity in the last 24 hours, and deliver a ranked list of 5 content angles.
+This should help me stay on top of the hottest workflows people are cooking in AI.
+12:39 AM ✓✓
+# youtube automate
+This is my YouTube channel. Help me figure out a YouTube workflow that I can use with this Hermes agent. I want to create a swarm of agents for my channel.
+Help me figure out the names for these agents, and let's set up these agents first. Tell me which agent will work for what, and then let's build these agents and run them.
+# if you are happy with that agent plan
+Build me all of them one by one, then let me know so that we can do a test run.
+# pitch deck that sells
+Create me a presentation deck for the latest Claude for small businesses.
+My pitch will be to small businesses on how I can help set up automations for them.
+Set up a presentation deck for this, and also guide me on the top five high-paying
+clients for these small businesses that I should target
+# one person company
+Hey, I am a founder and I want to start a one-person company using Hermes. Can you suggest what all I can build using Hermes?
+Hey, so I am a founder and I want to start a one-person company using Hermes. 
+Can you suggest me what all I can build using Hermes? As a solopreneur"
+
+
+#example
+hey, set up an agent that scrapes the most popular dentists in Bangalore HSR, and let's create them a professional,
+minimal, high-conversion website.1. First, find the most known and most popular dentist in Bangalore HSR.
+2. Build them a professional, minimal, high-conversion website.
+3. Find all their addresses (email address, phone number, etc.) so we can contact them and draft an email as well.
+4. Run this task in parallel using some sub-agents as required,
